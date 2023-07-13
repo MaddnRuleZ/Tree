57258834057e3cf47e8e0d9fc71638876b517d8d
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2023-07-13 15:46:24 +0200

    addet TestCases

--- SpringApplication/src/main/java/com/Application/Application.java
+++ SpringApplication/src/main/java/com/Application/Application.java
@@ -1,5 +1,7 @@
 package com.Application;
 
+import com.Application.Interpreter.Parser;
+import com.Application.Tree.elements.sectioning.Root;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,23 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		boolean maddinInTheHouse = true;
+
+		if (maddinInTheHouse) {
+			// start test Suite, kannst darunter dein Test code machen, set false für einfaches ausschalten
+			Parser parser = new Parser("SpringApplication/src/MaddinIsTesting/PSE_TEST_2.txt");
+			Root root = parser.startParsing();
+
+
+			String[] finishedTextReCompiled = root.toTextAdvanced();
+			for (String str: finishedTextReCompiled) {
+				System.out.println(str);
+			}
+
+
+		}
+
 
 	}
 }
