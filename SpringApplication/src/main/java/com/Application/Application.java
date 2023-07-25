c0ef8b7b41383b6ebacdc9e9afe3ac5fe9f0dde8
svMahler <svMahler@student.kit.edu>
2023-07-25 16:50:03 +0200

    CrossOrigin

--- SpringApplication/src/main/java/com/Application/Application.java
+++ SpringApplication/src/main/java/com/Application/Application.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * main class of the application
@@ -18,6 +21,21 @@ public class Application {
 	@Autowired
 	private User user;
 	private Clock clock;
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200") // Or you can use "*" to allow all origins
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // Or you can use "*" to allow all methods
+						.allowedHeaders("Content-Type", "Authorization") // Or you can use "*" to allow all headers
+						.allowCredentials(true);
+			}
+		};
+	}
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
 	}
