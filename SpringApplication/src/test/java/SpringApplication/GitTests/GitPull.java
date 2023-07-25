c95319fa0c1d8820aa684d89e0197e6ccd460cb9
ueteb <xmaddinhd@gmail.com>
2023-07-25 16:31:32 +0200

    git connection Mk2

--- SpringApplication/src/test/java/SpringApplication/GitTests/GitPull.java
+++ SpringApplication/src/test/java/SpringApplication/GitTests/GitPull.java
@@ -5,34 +5,35 @@ import org.junit.jupiter.api.Test;
 
 public class GitPull {
     private String wokringDirectory = "src/TestDocuments/gitTesting/";
+    private String overleafConnectionString; // = //"https://git.overleaf.com/64b430167d4b3be6afb4389c";
+
+
     @Test
     public void gitPushTest() {
-        GitPrinter gitPrinter = new GitPrinter("https://git.overleaf.com/64b430167d4b3be6afb4389c",
+        GitPrinter gitPrinter = new GitPrinter(overleafConnectionString,
                 "ueteb@student.kit.edu", "WhatWasMyPass??", wokringDirectory);
         gitPrinter.pushChanges();
     }
 
     @Test
     public void gitPullTest() {
-        GitPrinter gitPrinter = new GitPrinter("https://git.overleaf.com/64b430167d4b3be6afb4389c",
+        GitPrinter gitPrinter = new GitPrinter(overleafConnectionString,
                 "ueteb@student.kit.edu", "WhatWasMyPass??",wokringDirectory);
         gitPrinter.pullRepository();
     }
 
     @Test
     public void gitCloneTest() {
-        GitPrinter gitPrinter = new GitPrinter("https://git.overleaf.com/64b430167d4b3be6afb4389c",
+        GitPrinter gitPrinter = new GitPrinter(overleafConnectionString,
                 "ueteb@student.kit.edu", "WhatWasMyPass??", wokringDirectory);
         gitPrinter.cloneRepository();
     }
 
 
     public void gitRebaseTest() {
-        GitPrinter gitPrinter = new GitPrinter("https://git.overleaf.com/64b430167d4b3be6afb4389c",
+        GitPrinter gitPrinter = new GitPrinter(overleafConnectionString,
                 "ueteb@student.kit.edu", "WhatWasMyPass??", wokringDirectory);
         gitPrinter.rebaseChanges("master/origin");
     }
-
-
 
 }
