5dbcab93baa9be2268445f7266a2039514a04885
ueteb <xmaddinhd@gmail.com>
2023-07-25 15:46:21 +0200

    Docu & QOL

--- SpringApplication/src/test/java/SpringApplication/GitTests/GitPull.java
+++ SpringApplication/src/test/java/SpringApplication/GitTests/GitPull.java
@@ -5,10 +5,30 @@ import org.junit.jupiter.api.Test;
 
 
 public class GitPull {
+    private String wokringDirectory = "C:\\Users\\xmadd\\Desktop\\pullTesting";
+
+
     @Test
     public void gitPushTest() {
+        GitPrinter gitPrinter = new GitPrinter(null,"https://git.overleaf.com/64b430167d4b3be6afb4389c",
+                "ueteb@student.kit.edu", "WhatWasMyPass?", wokringDirectory);
+        gitPrinter.pushChanges();
+    }
+
+
+    public void gitPullTest() {
+
         GitPrinter gitPrinter = new GitPrinter(null,"https://git.overleaf.com/64b430167d4b3be6afb4389c",
                 "ueteb@student.kit.edu", "WhatWasMyPass?","src/GitDirectoryTests/Overleaf_1/");
         gitPrinter.pushChanges();
     }
+
+
+    public void gitCloneTest() {
+
+        GitPrinter gitPrinter = new GitPrinter(null,"https://git.overleaf.com/64b430167d4b3be6afb4389c",
+                "ueteb@student.kit.edu", "WhatWasMyPass?","src/GitDirectoryTests/Overleaf_1/");
+        gitPrinter.pushChanges();
+    }
+
 }
