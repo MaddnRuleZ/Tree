f8585e3b5dccaa35e304e4cec29fc4283d39d785
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2023-08-01 18:24:22 +0200

    Documentation

--- SpringApplication/src/test/java/SpringApplication/ParserTests/ParserTest.java
+++ SpringApplication/src/test/java/SpringApplication/ParserTests/ParserTest.java
@@ -37,16 +37,67 @@ public class ParserTest {
         System.out.println(map.get("root").toString());
     }
 
+
+    @Test
+    public void testParserAdvanced() throws UnknownElementException {
+        /*
+         * test the Parser and check if the file in sys.out is like the input_file below >>>
+         */
+        Parser parser = new Parser("src/test/resources/TestDocuments/PSE_TEST_2.txt");
+        Root root = null;
+
+        try {
+            root = (Root) parser.startParsing();
+        } catch (FileInvalidException e) {
+
+        }
+        if (root == null) {
+            System.out.println("Root null, document not correctly loaded");
+            return;
+        }
+
+        Map<String, StringBuilder> map = new HashMap<>();
+        map.put("root", new StringBuilder());
+        root.toLaTeX(map, "root", 0);
+        System.out.println(map.get("root").toString());
+    }
+
+
+
+    @Test
+    public void environmentTest() throws UnknownElementException {
+        /*
+         * test the Parser and check if the file in sys.out is like the input_file below >>>
+         */
+        Parser parser = new Parser("src/test/resources/TestDocuments/PSE_TEST_ENV.txt");
+        Root root = null;
+
+        try {
+            root = (Root) parser.startParsing();
+        } catch (FileInvalidException e) {
+            System.out.println("Invalid File Used");
+        }
+        if (root == null) {
+            System.out.println("Root null, document not correctly loaded");
+            return;
+        }
+
+        Map<String, StringBuilder> map = new HashMap<>();
+        map.put("root", new StringBuilder());
+        root.toLaTeX(map, "root", 0);
+        System.out.println(map.get("root").toString());
+    }
+
+
     @Test
     public void doubleParserTest() throws FileInvalidException {
         /*
          * test the Parser and check if the file in sys.out is like the input_file below >>>
          */
-        Parser parser = new Parser("src/test/resources/TestDocuments/PSE_TEST_1.txt");
+        Parser parser = new Parser("src/test/resources/TestDocuments/ENV_TST.txt");
         parser.initStartParsing();
         Root root = null;
         try {
-
             root = (Root) parser.startParsing();
         } catch (FileInvalidException e) {
             throw new RuntimeException(e);
