133a37fd606ef5d3dcd70c47cbdabe2775400e65
svMahler <svMahler@student.kit.edu>
2023-08-23 11:41:27 +0200

    figure und ApplicationConfig angepasst

--- SpringApplication/src/main/java/com/application/ApplicationConfig.java
+++ SpringApplication/src/main/java/com/application/ApplicationConfig.java
@@ -49,4 +49,19 @@ public class ApplicationConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
 }
